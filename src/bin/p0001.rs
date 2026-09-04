81b64c788796da629bbd293f1e31c1b137977972
josephcassman <dev@null>
2026-09-04 13:43:40 -0700

    Added another way to solve problem 1

--- src/bin/p0001.rs
+++ src/bin/p0001.rs
@@ -11,19 +11,53 @@
 //!
 
 fn main () {
+    println!("\n iterative method: {}", iterative());
+    println!("\n formula method: {}", formula());
+}
+
+/// Straightforward iterative method, 𝛰(n)
+fn iterative () -> u32 {
     let mut sum: u32 = 0;
+    for n in 1..1_000 {
+        if n % 3 == 0 || n % 5 == 0 {
+            sum += n;
+        }
+    }
+    sum
+}
 
-    let mut three: u32 = 3;
-    while three < 1_000 {
-        sum += three;
-        three += 3;
+/// We can use the triangle numbers as a base.
+/// The following formula gets the sum from 1 to 'n:
+///
+///    T(n) = n * (n + 1) / 2
+///
+/// Multiplying T(n) by three or five shifts the
+/// sequence to the sum of each multiple sequence.
+/// However, in doing so the total will be greater
+/// than 1_000, so 'n must be reduced accordingly.
+///
+/// Since we want to find all multiples of 'k less than 'n,
+/// we have the inequality m * k < n. Since these values
+/// are integers, solving for 'm, we have the following:
+///
+///    m * k ≤ n - 1
+///    m ≤ (n - 1) / k
+///    m = floor((n - 1) / k)
+///
+/// Then call 'U the triangle function shifted by 'k:
+///
+///    U(n, k) = k * m * (m + 1) / 2
+///
+/// Finally, common multiples must be removed since
+/// they have already been included:
+///
+///    sum = U(1000, 3) + U(1000, 5) - U(1000, 15)
+///
+fn formula () -> u32 {
+    fn u (k: u32) -> u32 {
+        let m = (1_000 - 1) / k;
+        k * m * (m + 1) / 2
     }
 
-    let mut five: u32 = 5;
-    while five < 1_000 {
-        if five % 3 != 0 { sum += five; }
-        five += 5;
-    }
-
-    println!("\n{}", sum);
+    u(3) + u(5) - u(15)
 }
