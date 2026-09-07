92d4d2a91b1e8d4577f0a10100a1bfc30b7cba89
josephcassman <dev@null>
2026-09-06 19:22:51 -0700

    Improve the performance and accuracy of the Pythagorean triple iterators

--- src/sequences/pythagorean_triples.rs
+++ src/sequences/pythagorean_triples.rs
@@ -16,9 +16,61 @@ pub type Triple = (u64, u64, u64);
 ///    b = 2·m·n
 ///    c = m² + n²
 ///
-pub struct PrimitivePythagoreanTriples { m: u64, n: u64 }
-impl PrimitivePythagoreanTriples { pub fn new () -> Self { Self { m: 2, n: 1 } } }
-impl Default for PrimitivePythagoreanTriples { fn default () -> Self { Self::new() } }
+pub struct PrimitivePythagoreanTriples {
+    m: u64,
+    n: u64,
+    limit: u64,
+    max_m: u64,
+}
+
+impl PrimitivePythagoreanTriples {
+    // Keep both a and b less than or equal to limit.
+    pub fn new (limit: u64) -> Self {
+        // Cut off the sequence when a or b is above limit.
+        //
+        // To perform this check efficiently, we can observe
+        // that a is small if n is large and conversely
+        // b is small if n is small. This means max(a, b)
+        // is found when a and b are roughly equal:
+        //
+        //    m² - n² ≈ 2·m·n                          # approximation
+        //
+        //    m² - n² = 2·m·n                          # 1: Consider the approximation as an equation
+        //    n² + 2·m·n - m² = 0                      # 2: Rearrange terms
+        //    n²/m² + 2·m·n/m² - m²/m² = 0             # 3: Multiply by 1/m²
+        //    n²/m² + 2·n/m - 1 = 0                    # 4: Simplify
+        //    x² + 2·x - 1 = 0                         # 5: Let x = n/m
+        //    x = -1 ± √2                              # 6: Solve using the quadratic formula
+        //    x = -1 + √2                              # 7: Discard negative root since m > 0 and n ≥ 1
+        //    n/m = √2 - 1                             # 8: Substitute back in for x
+        //    n = (√2 - 1)·m                           # 9: Solve for n
+        //
+        //    n ≈ (√2 - 1)·m ≈ 0.41421·m               # 10: Approximate n
+        //
+        //    b = 2·m·n                                # 11: Definition of b
+        //      ≈ 2·(√2 - 1)·m² ≡ A ≈ 0.82843·m²       # 12: Use the approximation from 10
+        //
+        // So once A is greater than or equal to the limit value,
+        // no value for n can keep a and b under the limit.
+        // That is, a value of m that satisfies the following
+        // inequalities means b is greater than the limit.
+        //
+        //    A > LIMIT
+        //    2·(√2 - 1)·m² > LIMIT
+        //    m > √(LIMIT/(2·(√2 - 1))) ≈ 1.09868·√LIMIT
+        //
+        let max_m = ((limit as f64) / (2.0 * (core::f64::consts::SQRT_2 - 1.0))).sqrt().floor() as u64;
+
+        Self {
+            m: 2,
+            n: 1,
+            limit,
+            max_m,
+        }
+    }
+}
+
+impl Default for PrimitivePythagoreanTriples { fn default () -> Self { Self::new(1_000) } }
 
 impl Iterator for PrimitivePythagoreanTriples {
     type Item = Triple;
@@ -26,9 +78,23 @@ impl Iterator for PrimitivePythagoreanTriples {
     fn next (&mut self) -> Option<Self::Item> {
         use crate::discrete_math::gcd;
 
+        let limit = self.limit;
         loop {
+            if self.m > self.max_m { return None; }
+
             let m = self.m;
             let n = self.n;
+
+            // Filter out b above the limit.
+            // Performing this check here saves effort and
+            // is possible since any subsequent value of n
+            // will also produce a value above the limit.
+            let b = 2 * m * n;
+            if b > limit {
+                self.m += 1;
+                self.n = 1;
+                continue;
+            }
 
             // Advance m and n while maintaining constraint 1.
             self.n += 1;
@@ -45,11 +111,11 @@ impl Iterator for PrimitivePythagoreanTriples {
                 continue;
             }
 
-            return Some((
-                m * m - n * n,  // a
-                2 * m * n,      // b
-                m * m + n * n,  // c
-            ));
+            // Now filter out values of a greater the limit.
+            let a = m * m - n * n;
+            if a > limit { continue; }
+
+            return Some((a, b, m * m + n * n));
         }
     }
 }
@@ -61,20 +127,19 @@ pub struct PythagoreanTriples {
     inner: PrimitivePythagoreanTriples,
     cur: Option<Triple>,
     k: u64,
-    a_limit: u64,
+    limit: u64,
 }
 
 impl PythagoreanTriples {
-    /// req: a_limit > 3
-    pub fn new (a_limit: u64) -> Self {
-        let mut inner = PrimitivePythagoreanTriples::new();
+    pub fn new (limit: u64) -> Self {
+        let mut inner = PrimitivePythagoreanTriples::new(limit);
         let cur = inner.next();
 
         Self {
             inner,
             cur,
             k: 1,
-            a_limit,
+            limit,
         }
     }
 }
@@ -88,19 +153,11 @@ impl Iterator for PythagoreanTriples {
         let Some((a, b, c)) = self.cur else { return None; };
 
         let k = self.k;
-        if a * k > self.a_limit {
+        if a * k > self.limit || b * k > self.limit {
             // Set to 2 instead of 1 since the k == 1
             // case is returned on this pass.
             self.k = 2;
-
-            while let Some(x) = self.inner.next() {
-                let (next_a, ..) = x;
-                if next_a < self.a_limit {
-                    self.cur = Some(x);
-                    break;
-                }
-            }
-
+            self.cur = self.inner.next();
             self.cur
         }
         else {
@@ -145,20 +202,23 @@ mod tests {
 
             (5, 12, 13),   // k == 1
             (10, 24, 26),  // k == 2
-            (15, 36, 39),  // k == 3
-            (20, 48, 52),  // k == 4
+            // (15, 36, 39),  // k == 3, not included because b > 25
 
             (15, 8, 17),   // k == 1
+            // (30, 16, 34),   // k == 2, not included because a > 25
 
             (7, 24, 25),   // k == 1
-            (14, 48, 50),  // k == 1
+            // (14, 48, 50),  // k == 2, not included because b > 25
 
-            // (21, 20, 29) excluded since a == 21 > 20
+            (21, 20, 29)   // k == 1
+            // (42, 40, 58)   // k == 2, not included because a and b > 25
 
-            (9, 40, 41),   // k == 1
-            (18, 80, 82),  // k == 2
+            // (9, 40, 41),   // k == 1, not included because b > 25
+            // (18, 80, 82),  // k == 1, not included because b > 25
+
+            // ... not more values satisfy a and b <= 25
         ];
-        let actual: Vec<Triple> = PythagoreanTriples::new(20).take(15).collect();
+        let actual: Vec<Triple> = PythagoreanTriples::new(25).take(11).collect();
 
         assert_eq!(actual, expected);
     }
