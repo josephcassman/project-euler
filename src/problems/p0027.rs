3cc80a6ae3e068dfb27a7579e4fac5c6f87c8325
josephcassman <dev@null>
2026-09-13 17:25:43 -0700

    Add a comment about the optimization level required to run the brute-force solution to problem 27

--- src/problems/p0027.rs
+++ src/problems/p0027.rs
@@ -35,6 +35,11 @@ pub fn run () {
     println!("\nbrute-force method: {:?}\n", brute_force());
 }
 
+///
+/// run with release mode on:
+///
+///    cargo run --release -- 27
+///
 fn brute_force () -> i64 {
     #[inline(always)]
     fn f (n: i64, a: i64, b: i64) -> i64 {
